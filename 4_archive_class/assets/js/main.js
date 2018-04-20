fea8164cd7f37f0c60706dbbd5760836592ef03f
Wang XInyi <ivywang@dyn-118-138-8-19.its.monash.edu.au>
2018-04-20 14:11:11 +1000

    commit

---
+++ 4_archive_class/assets/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+$('.b-red').click(function(){
+    $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+    $('.item.red').addClass('show');
+    $('.item.red').removeClass('hide');
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ 5_archive/assets/js/main.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+
+$('.b-red').click(function(){
+    $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+    $('.item.red').addClass('show');
+    $('.item.red').removeClass('hide');
+
+    });
+
+
+    $('.b-chinese').click(function(){
+        $('.item').addClass('hide');
+      $('.item').removeClass('show');
+
+        $('.item.chinese').addClass('show');
+        $('.item.chinese').removeClass('hide');
+
+        });
+
+        $('.b-vase').click(function(){
+            $('.item').addClass('hide');
+          $('.item').removeClass('show');
+
+            $('.item.vase').addClass('show');
+            $('.item.vase').removeClass('hide');
+
+            });
+
+            $('.b-bowl').click(function(){
+                $('.item').addClass('hide');
+              $('.item').removeClass('show');
+
+                $('.item.bowl').addClass('show');
+                $('.item.bowl').removeClass('hide');
+
+                });
+
+                $('.b-plate').click(function(){
+                    $('.item').addClass('hide');
+                  $('.item').removeClass('show');
+
+                    $('.item.plate').addClass('show');
+                    $('.item.plate').removeClass('hide');
+
+                    });
+
+                    $('.b-figure').click(function(){
+                        $('.item').addClass('hide');
+                      $('.item').removeClass('show');
+
+                        $('.item.figure').addClass('show');
+                        $('.item.figure').removeClass('hide');
+
+                        });
+
+                        $('.b-accesories').click(function(){
+                            $('.item').addClass('hide');
+                          $('.item').removeClass('show');
+
+                            $('.item.accesories').addClass('show');
+                            $('.item.accesories').removeClass('hide');
+
+                            });
+
+                            $('.b-pair').click(function(){
+                                $('.item').addClass('hide');
+                              $('.item').removeClass('show');
+
+                                $('.item.pair').addClass('show');
+                                $('.item.pair').removeClass('hide');
+
+                                });
+
+                                $('.b-blue').click(function(){
+                                    $('.item').addClass('hide');
+                                  $('.item').removeClass('show');
+
+                                    $('.item.blue').addClass('show');
+                                    $('.item.blue').removeClass('hide');
+
+                                    });
+
+                                    $('.b-colorful').click(function(){
+                                        $('.item').addClass('hide');
+                                      $('.item').removeClass('show');
+
+                                        $('.item.colorful').addClass('show');
+                                        $('.item.colorful').removeClass('hide');
+
+                                        });
+
+
+
+
+
+
+
+
+
+});

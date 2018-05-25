c9fb6c38dd20210b09ac3fb5ece3f5b1bae92796
Wang XInyi <ivywang@dyn-118-138-16-78.its.monash.edu.au>
2018-05-25 14:14:44 +1000

    upload

---
+++ 8_reader/assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  // Add jQuery here
+
+  // On .nav click, toggleClass ON or OFF showme.
+
+$('.nav').click(function() {
+  $('.menu').toggleClass('showme');
+});
+
+
+});
+
+// Add jQuery here
